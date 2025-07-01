811a4898aa3ec7dfd02941c81cf6541bf26a9953
Aly <aly.kazani52@gmail.com>
2025-07-01 20:33:59 +0500

    Initial Commit - testing git cli

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//CGPA calculator
+// This program calculates the CGPA based on user input for grades and credits.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void displayLogo(){
+    cout << "==========================================" << endl;
+    cout << "             CGPA Calculator              " << endl;
+    cout << "                  By Aly                  " << endl;
+    cout << "==========================================" << endl;
+}
+
+int main(){
+    int choice;
+
+
+
+    return 0;
+}
